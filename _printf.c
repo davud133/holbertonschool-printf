d07f1446d16b6b5ed0ad1dcb77ab7e655b5d9f2a
davud <davudgeroqiq@gmail.com>
2026-01-11 16:25:46 +0300

    new commit

---
+++ _printf.c
@@ -0,0 +1,59 @@
+#include "main.h"
+#include <unistd.h>
+#include <stdarg.h>
+/**
+ * _printf - my own printf
+ * @format: string
+ * @...: arguments
+ *
+ * Return: nothing
+ */
+int _printf(const char *format, ...)
+{
+	va_list arg;
+	char *Str = format;
+	int i;
+	char c;
+	char *s;
+	int k;
+	int counter = 0;
+	va_start(arg, format);
+	for (i = 0; Str[i] != '\0'; i++)
+	{
+		if (Str[i] == '%')
+		{
+			if (Str[i + 1] == 'c')
+			{
+				c = va_arg(arg, int);
+				write(1, &c, 1);
+				i += 1;
+				counter++;
+			}
+			else if (Str[i + 1] == 's')
+
+			{
+				s = va_arg(arg, char *);
+				if (s == NULL)
+					s = "(null)";
+				for (k = 0; s[k] != '\0'; k++)
+				{
+					write(1, &s[k], 1);
+					counter++;
+				}
+				i += 1;
+			}
+			else
+			{
+				write(1, &Str[i], 1);
+				counter++;
+			}
+		}
+		else
+		{
+			write(1, &Str[i], 1);
+			counter++;
+		}
+	}
+	va_end(arg);
+	return (counter);
+}
